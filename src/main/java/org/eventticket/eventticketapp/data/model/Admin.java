d60e5433b67839cf749fb94c04f6d4dfa7fba2ff
emmabobby <evansjota@gmail.com>
2024-09-26 17:55:39 +0100

    configure app.properties

--- src/main/java/org/eventticket/eventticketapp/data/model/Admin.java
+++ src/main/java/org/eventticket/eventticketapp/data/model/Admin.java
@@ -1,2 +1,16 @@
-package org.eventticket.eventticketapp.data.model;public class Admin {
+package org.eventticket.eventticketapp.data.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Admin {
+    @Id
+    private int id;
+    private String username;
+    private String password;
 }
---
+++ src/main/java/org/eventticket/eventticketapp/data/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package org.eventticket.eventticketapp.data.repository;
+
+import org.eventticket.eventticketapp.data.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
